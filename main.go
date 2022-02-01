3161930d5ca3a1ba4aa7cb9108a0ad8ac08f3055
Gavriel Rousine <gavriel.gav@gmail.com>
2022-02-01 20:55:02 +0200

    fixed module name and tested struct constructor

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/gavriel200/goku/server"
+)
+
+func main() {
+	server := server.NewServer("test")
+
+	fmt.Println(server.Name)
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,9 @@
 package server
+
+type Server struct {
+	Name string
+}
+
+func NewServer(Name string) *Server {
+	return &Server{Name}
+}
